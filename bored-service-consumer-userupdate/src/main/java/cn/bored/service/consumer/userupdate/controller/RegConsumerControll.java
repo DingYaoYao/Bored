d230215158dafbe4d5ea093a9916b9a8a2178617
DingYaoYao <3558488355@qq.com>
2019-07-03 14:32:03 +0800

    七天

--- bored-service-consumer-userupdate/src/main/java/cn/bored/service/consumer/userupdate/controller/RegConsumerControll.java
+++ bored-service-consumer-userupdate/src/main/java/cn/bored/service/consumer/userupdate/controller/RegConsumerControll.java
@@ -3,13 +3,12 @@ import cn.bored.common.dto.AbstractBaseResult;
 import cn.bored.domain.User;
 import cn.bored.service.consumer.userupdate.service.RegConsumerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import javax.servlet.http.HttpServletRequest;
 
 @RestController
+@RequestMapping("user")
 public class RegConsumerControll {
 
     @Autowired
