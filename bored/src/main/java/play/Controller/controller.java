959d71d5751ab1f0ee68f6785c007375c9e89620
DingYaoYao <3558488355@qq.com>
2019-07-13 14:31:07 +0800

    十七天

---
+++ bored/src/main/java/play/Controller/controller.java
@@ -0,0 +1,32 @@
+package play.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class controller {
+   // @GetMapping(value = {"/index",""})
+   @RequestMapping(value = {"/index",""}, method = RequestMethod.GET)
+    public String index(){
+        return "index";
+    }
+
+    @RequestMapping(value = {"/user"}, method = RequestMethod.GET)
+    public String user(){
+        return "user";
+    }
+    @RequestMapping(value = {"/users"}, method = RequestMethod.GET)
+    public String users(){
+        return "users";
+    }
+    @RequestMapping(value = {"/home"}, method = RequestMethod.GET)
+    public String home(){
+        return "home";
+    }
+
+    @RequestMapping(value = {"/pal"}, method = RequestMethod.GET)
+    public String pal(){
+        return "pal";
+    }
+}
